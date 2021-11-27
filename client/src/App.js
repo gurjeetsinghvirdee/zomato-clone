24f7133e7e5a4cc510fc08241f53ca6202bcf4e2
gurjeetsinghvirdee <gurjeetsinghvirdeee@gmail.com>
2021-11-27 11:46:04 +0530

    solved cors issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Home from "./App/Home";
+import Login from "./App/Login";
+import Signup from "./App/Signup";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Zomato</h1>
-    </div>
-  )
-}
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ const express = require("express");
 const app = express();
 const router = require("./routes");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 mongoose
   .connect("mongodb://localhost:27017/zomato_clone")
-  .then((res) => console.log("✨✨ Mongo Launched"))
-  .catch((error) => console.log("Unable to Launch Mongo"));
+  .then((res) => console.log("===>> Connected to DB"))
+  .catch((error) => console.log("Unable to connect DB"));
 
 const PORT = 2000;
 
 app.use(express.json());
 
+app.use(cors());
+
 app.use("/api", router);
 
 app.listen(PORT, () => {
-  console.log("🚀🚀 Server connected");
+  console.log("===>> Server is connected");
 });
