654d5baec204f4b9e9de977e1f86dbea7583d9d6
gurjeetsinghvirdee <gurjeetsinghvirdeee@gmail.com>
2021-11-28 16:00:04 +0530

    commit

---
+++ src/App/Home/Home.js
@@ -0,0 +1,14 @@
+import { dividerClasses } from '@mui/material';
+import React from 'react';
+import './Home.css';
+
+
+const Home = () => {
+     return(
+         <div className='column'>
+           h1
+         </div>
+     )
+}
+
+export default Home;
