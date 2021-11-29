b3a6bb15a504d46274fc0a69999878d198bb85b8
gurjeetsinghvirdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2021-11-29 12:48:20 +0530

    added css in home page

--- src/App/Home/Home.js
+++ src/App/Home/Home.js
@@ -3,7 +3,16 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <h1>Zomato</h1>
+    <div className="hero-container">
+      <div className="container">
+        <nav className='navbar'>
+          <li>Get the App</li>
+          <li>Add Resturant</li>
+          <li>Login</li>
+          <li>Sign Up</li>
+        </nav>
+      </div>
+    </div>
   )     
 }
 
